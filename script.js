6c689556a61cfd217f405d2aa31bbeb5a6a6b11a
byohannes <42332389+byohannes@users.noreply.github.com>
2020-05-09 22:09:10 +0100

    update

--- script.js
+++ script.js
@@ -1,59 +1,68 @@
-const search = document.querySelector('#searchInput')
-
+const search = document.querySelector('#search-input')
 const rootElm = document.querySelector('#root')
+const divContainer = document.querySelector('#show')
 const url = `https://restcountries.eu/rest/v2/all`
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => makePageForCountries(data))
-  .catch((error) => console.log(error))
-let countries
-function makePageForCountries(countriesData) {
-  countries = countriesData
-  for (let i = 0; i < countries.length; i++) {
-    let divResponsive = document.createElement('div')
-    divResponsive.className +=
-      'col-sm-12 col-md-4 col-lg-3 mb-sm-2 mb-md-2 mt-md-3 mb-lg-3 mt-lg-3 p-2 page'
-    let countryCard = document.createElement('div')
-    countryCard.className = 'card'
-    let flag = document.createElement('img')
-    flag.className = 'card-img-top'
-    flag.src = countries[i].flag
-    flag.addEventListener('click', () => {
-      displayInfo(countries[i])
-    })
-    let countriesInfo = document.createElement('div')
-    countriesInfo.className = 'card-body'
-    let countryName = document.createElement('h5')
-    countryName.className = 'card-title'
-    countryName.innerHTML = countries[i].name
-    let population = document.createElement('p')
-    population.className = 'card-text'
-    population.innerHTML = 'Population: ' + countries[i].population
-    let region = document.createElement('p')
-    region.className = 'card-text'
-    region.innerHTML = 'Region: ' + countries[i].region
-    let capital = document.createElement('p')
-    capital.className = 'card-text'
-    capital.innerHTML = 'Capital: ' + countries[i].capital
-    countryCard.appendChild(flag)
-    countriesInfo.appendChild(countryName)
-    countriesInfo.appendChild(population)
-    countriesInfo.appendChild(region)
-    countriesInfo.appendChild(capital)
-    countryCard.appendChild(countriesInfo)
-    divResponsive.appendChild(countryCard)
-    rootElm.appendChild(divResponsive)
-  }
+window.onload = setup
+
+function setup() {
+    getData()
 }
+
+function getData() {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => makePageForCountries(data))
+        .catch((error) => console.log(error))
+}
+let countries
+
+function makePageForCountries(countriesData) {
+    countries = countriesData
+    for (let i = 0; i < countries.length; i++) {
+        let divResponsive = document.createElement('div')
+        divResponsive.className +=
+            'col-sm-12 col-md-4 col-lg-3 mb-sm-2 mb-md-2 mt-md-3 mb-lg-3 mt-lg-3 p-2 page'
+        let countryCard = document.createElement('div')
+        countryCard.className = 'card'
+        let flag = document.createElement('img')
+        flag.className = 'card-img-top'
+        flag.src = countries[i].flag
+        flag.addEventListener('click', () => {
+            displayInfo(countries[i])
+        })
+        let countriesInfo = document.createElement('div')
+        countriesInfo.className = 'card-body'
+        let countryName = document.createElement('h5')
+        countryName.className = 'card-title'
+        countryName.innerHTML = countries[i].name
+        let population = document.createElement('p')
+        population.className = 'card-text'
+        population.innerHTML = 'Population: ' + countries[i].population
+        let region = document.createElement('p')
+        region.className = 'card-text'
+        region.innerHTML = 'Region: ' + countries[i].region
+        let capital = document.createElement('p')
+        capital.className = 'card-text'
+        capital.innerHTML = 'Capital: ' + countries[i].capital
+        countryCard.appendChild(flag)
+        countriesInfo.appendChild(countryName)
+        countriesInfo.appendChild(population)
+        countriesInfo.appendChild(region)
+        countriesInfo.appendChild(capital)
+        countryCard.appendChild(countriesInfo)
+        divResponsive.appendChild(countryCard)
+        rootElm.appendChild(divResponsive)
+    }
+}
+
 function displayInfo(country) {
-  console.log(country)
-  document.querySelector('#show').style.display = 'none'
-  const countryInfo = document.querySelector('#info')
-  const infoBody = countryInfo.querySelector('#info-body')
-  const countryImage = countryInfo.querySelector('img')
-  countryImage.className = 'countryImg'
-  countryImage.src = country.flag
-  infoBody.innerHTML = `
+    divContainer.innerHTML = ''
+    const countryInfo = document.querySelector('#info')
+    const infoBody = countryInfo.querySelector('#info-body')
+    const countryImage = countryInfo.querySelector('img')
+    countryImage.className = 'countryImg'
+    countryImage.src = country.flag
+    infoBody.innerHTML = `
         <h2 class="mt-sm-3">
         ${country.name}
         </h2>
@@ -103,44 +112,51 @@ function displayInfo(country) {
 }
 
 function findBorder(border) {
-  let borderCountry = ''
-  border.forEach((bor) => {
-    borderCountry += findCountryName(bor)
-  })
+    let borderCountry = ''
+    border.forEach((bor) => {
+        borderCountry += findCountryName(bor)
+    })
 }
 
 function findCountryName(alphaCode) {
-  console.log(countries)
-  console.log(countries.find((country) => country.alpha3code == alphaCode))
+    //console.log(countries)
+    // console.log(countries.find((country) => country.alpha3code == alphaCode))
 }
 // search Input
 search.addEventListener('input', findCountry)
 
 function findCountry() {
-  const inputValue = search.value.toLowerCase()
-  const pages = document.querySelectorAll('.page')
-  pages.forEach((ele) => {
-    if (ele.innerText.toLowerCase().indexOf(inputValue) >=0) {
-      ele.style.display = 'flex'
-    } else {
-      ele.style.display = 'none'
-    }
-  })
+    const inputValue = search.value.toLowerCase()
+    const pages = document.querySelectorAll('.page')
+    pages.forEach((ele) => {
+        if (ele.innerText.toLowerCase().indexOf(inputValue) >= 0) {
+            ele.style.display = 'flex'
+        } else {
+            ele.style.display = 'none'
+        }
+    })
 }
-
 // filter by continent
 const menu = document.querySelector('.dropdown-menu')
-const continents =document.querySelectorAll('.dropdown-item')
+const continents = document.querySelectorAll('.dropdown-item')
 continents.forEach((menu) => {
-  menu.addEventListener('click', () => {
-    const value = menu.innerText
-    const countryRegion = document.querySelectorAll('.page')
-    countryRegion.forEach((region) => {
-      if (region.innerText.includes(value)) {
-        region.style.display = 'flex'
-      } else {
-        region.style.display = 'none'
-      }
+        menu.addEventListener('click', () => {
+            const value = menu.innerText
+            const countryRegion = document.querySelectorAll('.page')
+            countryRegion.forEach((region) => {
+                if (region.innerText.includes(value)) {
+                    region.style.display = 'flex'
+                } else {
+                    region.style.display = 'none'
+                }
+            })
+        })
     })
-  })
+    // back button
+const backButton = document.querySelector('#back-button')
+backButton.addEventListener('click', () => {
+    backButton.style.display = 'none'
+    document.querySelector('#info').style.display = 'none'
+        //document.querySelector('#show').style.visibility = 'block'
+    getData()
 })
